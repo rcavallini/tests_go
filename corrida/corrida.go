bc67f80b9a5f12249cf0c405daebafd914b64c99
Renan Cavallini <renancavallini@gmail.com>
2025-05-01 21:18:24 -0300

    mensagens de entrada e de saida

--- corrida/corrida.go
+++ corrida/corrida.go
@@ -5,27 +5,28 @@ import (
 	"time"
 )
 
-func LargadaCorrida() {
-	go corrida("Ferrari")
-	go corrida("Mercedes")
-	go corrida("RBR")
-	go corrida("BMW")
-	go corrida("Renault")
+func LargadaCorrida(x int) {
+	go corrida("Ferrari", x)
+	go corrida("Mercedes", x)
+	go corrida("RBR", x)
+	go corrida("BMW", x)
+	go corrida("Renault", x)
 
 	//var input string
 	//fmt.Scanln(&input)
-	time.Sleep(15 * time.Second)
+	time.Sleep(time.Duration(x+2) * time.Second)
 }
 
-func corrida(carro string) {
+func corrida(carro string, x int) {
 	/*
 		Essa função vai simular o comportamento de uma carro em uma corrida de 30 voltas.
 		Vamos usar a função Sleep para gerar um "delay" de 1 segundo entre os prints.
 		Esses delays também serão utilizados pelo escalonador da linguagem como
 		critério de interrupção para alternancia entre as rotinas.
 	*/
-	for i := 0; i <= 10; i++ {
+	for i := 0; i <= x; i++ {
 		time.Sleep(1000 * time.Millisecond)
-		fmt.Println(fmt.Sprintf("%s : %d voltas", carro, i))
+		// fmt.Println(carro, ":", i, "voltas")
+		fmt.Printf("%s : %d voltas\n", carro, i)
 	}
 }
--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/rcavallini/tests_go/corrida"
 )
 
 func main() {
 	fmt.Println("Preparados! Foi dada a largada!")
-	corrida.LargadaCorrida()
+	var voltas int
+	fmt.Println("Quantas voltas a corrida terá?")
+	fmt.Scanln(&voltas)
+	corrida.LargadaCorrida(voltas) // Alterar para passar um numero inserido pelo usuario
 	fmt.Println("Corrida finalizada!")
+	var escuderia string
+	fmt.Println("Qual foi a escuderia vencedora?")
+	fmt.Scanln(&escuderia)
+	fmt.Println((strings.ToUpper(escuderia)))
 
 }
